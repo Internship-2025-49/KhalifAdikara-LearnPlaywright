2f1e620c6f5d87af119d66f3bdd82d379cb15679
Khahaha10 <knurfattah@gmail.com>
2025-01-30 22:57:39 +0700

    test: create a reusable locator in both test in todo.spec #1

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -7,20 +7,22 @@ test.describe('New Todo', () => {
   })
 
   test('active and completed filters', async ({ page }) => {
-    await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('water the plants.');
-    await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
-    await page.getByRole('textbox', { name: 'What needs to be done?' }).fill('feed the dog.');
-    await page.getByRole('textbox', { name: 'What needs to be done?' }).press('Enter');
+    const todoInput = page.getByPlaceholder('What needs to be done?')
+    await todoInput.fill('water the plants.');
+    await todoInput.press('Enter');
+    await todoInput.fill('feed the dog.');
+    await todoInput.press('Enter');
     await page.getByRole('listitem').filter({ hasText: 'water the plants.' }).getByLabel('Toggle Todo').check();
     await page.getByRole('link', { name: 'Active' }).click();
     await page.getByTestId('todo-title').click();
     await page.getByRole('link', { name: 'Completed' }).click();
     await page.getByTestId('todo-title').click();
+  });
 
-    test('text field is cleared when item is added', async ({ page}) => {
-      await page.getByPlaceholder('What needs to be done?').fill('water the plants');
-      await page.getByPlaceholder('What needs to be done?').press('Enter');
-      await expect(page.getByPlaceholder('What needs to be done?')).toBeEmpty();
-    })
+  test('text field is cleared when item is added', async ({ page}) => {
+    const todoInput = page.getByPlaceholder('What needs to be done?')
+    await todoInput.fill('water the plants');
+    await todoInput.press('Enter');
+    await expect(page.getByPlaceholder('What needs to be done?')).toBeEmpty();
   });
 });
