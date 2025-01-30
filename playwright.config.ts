5c85378b3d59c33c4e2e4349793828693a2d3e40
Khahaha10 <knurfattah@gmail.com>
2025-01-30 22:15:41 +0700

    test: add trace and screenshot in defineConfig use #1

--- playwright.config.ts
+++ playwright.config.ts
@@ -29,7 +29,8 @@ export default defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: 'on',
+    screenshot: 'on',
   },
 
   /* Configure projects for major browsers */
@@ -48,7 +49,7 @@ export default defineConfig({
       name: 'webkit',
       use: { ...devices['Desktop Safari'] },
     },
-    
+
     {
       name: 'Mobile Safari',
       use: { ...devices['iPhone 12 Pro'] },
